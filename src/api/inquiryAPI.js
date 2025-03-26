05a9b066e80a71c25a7045cd344d350e98b66c1d
Sayantan Dev <flaamant@gmail.com>
2025-03-26 14:12:08 +0530

    env updated

--- src/api/inquiryAPI.js
+++ src/api/inquiryAPI.js
@@ -1,9 +1,10 @@
 import axios from './interceptor';
 
-export const USER_URL = `${process.env.REACT_APP_BASE_URL}/inquiry`;
+// const USER_URL = `${process.env.REACT_APP_BASE_URL}/inquiry`;
+const INQR_URL = 'https://tour-backend-live.onrender.com/api/v1/inquiry';
 
 export async function getAllInquiries() {
-    const ADDUSER_URL = `${USER_URL}/get-all-inquiries`;
+    const ADDUSER_URL = `${INQR_URL}/get-all-inquiries`;
     const result = await axios.get(ADDUSER_URL);
     return result.data;
 };
